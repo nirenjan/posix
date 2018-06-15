6386bb034f758d45771eb9a7e31ac7ea385b40da
nirenjan <nirenjan@gmail.com>
2018-06-15 14:51:09 -0700

    Break true and false utilities into separate files
    This is necessary to install the correct symbolic links, instead of
    having a single link for true_false, which doesn't get recognized by the
    service.

--- src/handlers/false.c
+++ src/handlers/false.c
@@ -1,13 +1,7 @@
 /**********************************************************************
- true shall always return with a 0 exit status
  false shall always return with a non-zero exit status
  **********************************************************************
  */
-
-int posix_true(int argc, char **argv)
-{
-    return 0;
-}
 
 int posix_false(int argc, char **argv)
 {
